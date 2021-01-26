daabe29a00e6d6d9922509ca4f56da055300c32e
DELL <punyasmrutin@gmail.com>
2021-01-27 00:15:57 +0530

    modified files

--- src/main/java/com/in28minutes/microservices/netflixcloudapigatewayserver/config/GatewayConfig.java
+++ src/main/java/com/in28minutes/microservices/netflixcloudapigatewayserver/config/GatewayConfig.java
@@ -1,21 +1,19 @@
 package com.in28minutes.microservices.netflixcloudapigatewayserver.config;
 /*
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableHystrix
+//@EnableHystrix
 @Configuration
 public class GatewayConfig {
-	
+
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(
-						p -> p.path("/get")
-						.filters(f -> f.addRequestHeader("hello","world")
-						)
-						.uri("http://localhost:8081")).build();
+		return builder.routes().route(
+				r -> r.path("/currency-exchange-api/**").uri("http://localhost:8081").id("currency-exchange-service"))
+				.build();
 	}
-}
-*/
+}*/
